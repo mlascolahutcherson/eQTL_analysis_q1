e602f5d8db69abc343f8a4cc84549ef97a477e47
Maddie <maddie.alston15@gmail.com>
2022-12-05 21:51:08 -0800

    added final code

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 sys.path.insert(0, 'src')
 from etl import get_expressions, get_bim, get_bed
-from eda import generate_regression
+from eda import generate_regression, generate_pop_regression, bar_plot
 
 
 # In[ ]:
@@ -24,15 +24,26 @@ def main(targets):
     epressions_config = json.load(open('config/exp_config.json'))
     bim_config = json.load(open('config/bim_config.json'))
     bed_config = json.load(open('config/bed_config.json'))
+    reg_config = json.load(open('config/reg_config.json'))
+    bar_plot_config = json.load(open('config/bar_plot_config.json'))
     
-    if 'data' om targets:
+    if 'data' in targets:
         exp_pop = get_expressions(**expressions_config)
         genotype_df = get_bim(**bim_config)
         read = get_bed(**bed_config)
     
     if 'eda' in targets:
         generate_regression(exp_pop, genotype_df, read)
-    
+        
+        pop_dfs = []
+        for x in reg_config['pops']:
+            pop_dfs.append(exp_pop.loc[exp_pop['population']== x])
+        pop_specific = generate_pop_regression(pop_dfs, genotype_df, read, reg_config['sig_threshold'])
+        
+        to_plot = pop_specific.groupby(['pop']).count()
+        bar_plot(to_plot.index, to_plot['p'], **bar_plot_config)
+        
+        
     return 
 
 
--- src/eda.py
+++ src/eda.py
@@ -51,3 +51,61 @@ def generate_regression(exp_pop, genotype_df, read ):
             
     return output 
 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def generate_pop_regression(POP, genotype_df, read, sig_threshold):
+    for x in POP:
+        output_ = pd.DataFrame(columns = ['SNP_ID', 'GENE',"SLOPE", 'SE', 'p'])
+        genes = x['Gene_Symbol'].unique()
+        for g in genes: 
+            row = x.loc[x['Gene_Symbol'] == g]
+            coord = row['Coord'].iloc[0]
+            dist = abs(int(coord) - genotype_df['POS'])
+            cis_dist = dist[dist <= 500000].index
+            for i in cis_dist:
+                gene = row['Gene_Symbol'].iloc[0]
+                snp = read[np.s_[::,i]]
+                snp_id = bed.sid[i]
+                snp_df = pd.DataFrame(data = {'sample_id':samples, 'snp':snp})
+                vals = x.loc[(x.Gene_Symbol == gene)]
+                merged =  vals.merge(snp_df, on = 'sample_id')
+                slope, intercept, r, p, se = stats.linregress((merged['snp'].astype(np.float), merged['Value'].astype(np.float)))
+                geno_df = genotype_df[genotype_df['SNP_ID'] == snp_id]
+                if p < sig_threshold:
+                    output.loc[len(output.index)] = [snp_id, gene, slope, se, p]
+    return output 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def bar_plot(x, y, title, ylabel, xlabel, color, font)
+    plt.figure(figsize = (9,6))
+    plt.bar(x = all_pops.index, height = all_pops['p'], color = ['lavender','slateblue', 'mediumslateblue','darkslateblue', 'navy'])
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.rcParams.update({'font.size': font})
+    plt.show()
+    return
+
+
+# In[ ]:
+
+
+
+
